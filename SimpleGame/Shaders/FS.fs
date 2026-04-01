f63bcf83dbd56b90a238ac02b759a56f6ffbf574
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2026-04-01 10:13:41 +0900

    FS : 격자무늬 패턴

--- SimpleGame/Shaders/FS.fs
+++ SimpleGame/Shaders/FS.fs
@@ -2,16 +2,37 @@
 
 layout(location=0) out vec4 FragColor;
 in vec2 v_TPos;
+const float c_PI = 3.1415926535897932384626433832795;
+
+void Simple() {
+    vec3 color;
+    
+    if (v_TPos.x + v_TPos.y < 0.5) { 
+    	color = vec3(1.0, 0.0, 0.0); // Red
+    } 
+    else {
+    	color = vec3(0.0, 0.0, 1.0); // Blue
+    }
+    FragColor = vec4(color, 1.0);
+}
+void Pattern() {
+    // FragColor = vec4(v_TPos, 0.0, 1.0);
+    float lineCount = 10;
+    float lineWidth = 1;
+    lineCount = lineCount / 2;
+    lineWidth = 100 / lineWidth;
+    float per = -0.5 * c_PI;
+    float Xgrey = pow(
+                    abs(sin((v_TPos.x * 2 * c_PI +per) * lineCount))
+                        , lineWidth);
+    float Ygrey = pow(
+                    abs(sin((v_TPos.y * 2 * c_PI +per) * lineCount))
+                        , lineWidth);
+    float grey = Xgrey + Ygrey;
+    FragColor = vec4(grey);
+}
 
 void main()
 {
-	vec3 color;
-
-	if (v_TPos.x + v_TPos.y < 0.5) { 
-		color = vec3(1.0, 0.0, 0.0); // Red
-	} 
-	else {
-		color = vec3(0.0, 0.0, 1.0); // Blue
-	}
-	FragColor = vec4(color, 1.0);
+	Pattern();
 }
