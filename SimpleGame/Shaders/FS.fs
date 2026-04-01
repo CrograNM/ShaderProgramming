de2f2efede53a8fa33aca26871a0820ebb7aaf78
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2026-04-01 10:47:12 +0900

    FS : 원 그리기

--- SimpleGame/Shaders/FS.fs
+++ SimpleGame/Shaders/FS.fs
@@ -17,22 +17,42 @@ void Simple() {
 }
 void Pattern() {
     // FragColor = vec4(v_TPos, 0.0, 1.0);
-    float lineCount = 10;
+    float lineCountH = 2;
+    float lineCountV = 2;
     float lineWidth = 1;
-    lineCount = lineCount / 2;
+    lineCountH = lineCountH / 2;
+    lineCountV = lineCountV / 2;
     lineWidth = 100 / lineWidth;
     float per = -0.5 * c_PI;
     float Xgrey = pow(
-                    abs(sin((v_TPos.x * 2 * c_PI +per) * lineCount))
+                    abs(sin((v_TPos.x * 2 * c_PI +per) * lineCountH))
                         , lineWidth);
     float Ygrey = pow(
-                    abs(sin((v_TPos.y * 2 * c_PI +per) * lineCount))
+                    abs(sin((v_TPos.y * 2 * c_PI +per) * lineCountV))
                         , lineWidth);
     float grey = Xgrey + Ygrey;
     FragColor = vec4(grey);
 }
+void Circle() {
+    vec2 center = vec2(0.5, 0.5);
+    vec2 currentPos = v_TPos.xy;
+    float dist = distance(center, currentPos);
 
+    float radius = 0.5;
+    float outlineWidth = 0.01;
+    outlineWidth = outlineWidth / 2;
+
+    if (dist < radius - outlineWidth) {
+        discard;
+    } else if (dist > radius + outlineWidth) {
+        discard;
+    }
+    FragColor = vec4(0.0, 1.0, 1.0, 1.0); // Cyan
+}
+void SixAngleStar() {
+    
+} 
 void main()
 {
-	Pattern();
+	Circle();
 }
